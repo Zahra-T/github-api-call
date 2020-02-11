0f160cedead25916daf5228c0047b21ac6f92718
Zahra-T <zahratehraninasab@gmail.com>
2020-02-11 16:31:57 +0330

    entry point public static void Main() in Program class

---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+
+
+namespace ApiCall
+{
+    class Program
+    {
+        public static void Main(string [] args){
+            string command = args[0];
+            GithubApiCall apiCall = new GithubApiCall();
+
+            if(command == "repo"){
+                string username = args[1];
+                Console.WriteLine(apiCall.GetRepos(username));
+            }
+            else{
+                Console.WriteLine("command " + args[0] + " not found");
+            }
+        }
+
+ 
+    }
+}
